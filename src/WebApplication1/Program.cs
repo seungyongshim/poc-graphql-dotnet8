fec143266d2c3266cf93d48d7657316b2985fd43
SeungYongShim <syshim@me.com>
2023-09-05 21:35:09 +0900

    .

---
+++ src/WebApplication1/Program.cs
@@ -0,0 +1,31 @@
+using GraphQL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQL(b =>
+{
+    b.AddSystemTextJson();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.UseGraphQLPlayground();
+
+app.Run();
+
